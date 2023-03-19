48c036aa95919ae8bb69f13130f1bbf4794c2d88
Sayan Karmakar <ironheartz367@gmail.com>
2023-03-19 13:06:21 +0530

    Add files via upload

---
+++ modularTetris/components/const.py
@@ -0,0 +1,5 @@
+
+class Constants:
+    WIDTH = 420
+    HEIGHT = 720
+    EXTENDED_WIDTH = 200
---
+++ modularTetris/main.py
@@ -0,0 +1,6 @@
+from components import ui
+
+
+if __name__ == "__main__":
+    game = ui.UI()
+    game.start()
